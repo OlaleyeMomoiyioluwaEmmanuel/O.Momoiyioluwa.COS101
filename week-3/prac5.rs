56a945d157f5ea8e72c5bc1b05e4d5e16ed490e1
OlaleyeMomoiyioluwaEmmanuel <olaleyeemmanuel.t@gmail.com>
2025-10-22 21:54:31 +0100

    (my third commit)

---
+++ week-3/prac5.rs
@@ -0,0 +1,7 @@
+fn fn main() {
+	let special_character = '@';//default
+	let alphabet :char = 'B';
+	let special_character = "Bolaji"
+	let special_character = '@';//default
+	let special_character = '@';//default
+}
