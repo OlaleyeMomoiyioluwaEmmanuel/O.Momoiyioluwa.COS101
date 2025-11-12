53fd2ec7c3f8160608826a836169b132a016e2df
OlaleyeMomoiyioluwaEmmanuel <olaleyeemmanuel.t@gmail.com>
2025-11-12 17:19:11 +0100

    Delete Week-7 directory

--- Week-7/practice1/src/practice1 .rs
+++
@@ -1,8 +0,0 @@
-fn my_grade() {
-    //function body
-    println!("Gretings from function my_grade!");
-}
-fn fn main() {
-    //calling a function
-    my_grade!();
-}
--- Week-7/project1/target/debug/deps/project1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\DELL\OneDrive\Documents\O.Momoiyioluwa.COS101\Week-7\project1\target\debug\deps\project1.d: src\main.rs
-
-C:\Users\DELL\OneDrive\Documents\O.Momoiyioluwa.COS101\Week-7\project1\target\debug\deps\project1.exe: src\main.rs
-
-src\main.rs:
