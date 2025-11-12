58253e2251af9685378ec915ca964407aedbb309
OlaleyeMomoiyioluwaEmmanuel <olaleyeemmanuel.t@gmail.com>
2025-11-12 17:06:26 +0100

    (My second commit)

---
+++ Week-7/practice1/src/practice1 .rs
@@ -0,0 +1,8 @@
+fn my_grade() {
+    //function body
+    println!("Gretings from function my_grade!");
+}
+fn fn main() {
+    //calling a function
+    my_grade!();
+}
---
+++ Week-7/project1/target/debug/project1.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\OneDrive\Documents\O.Momoiyioluwa.COS101\Week-7\project1\target\debug\project1.exe: C:\Users\DELL\OneDrive\Documents\O.Momoiyioluwa.COS101\Week-7\project1\src\main.rs
